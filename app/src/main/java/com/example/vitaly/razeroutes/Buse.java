712e5368e312002cf17f17e5eebacb06b48d4f64
026rus <026rus@gmail.com>
2016-04-15 20:45:42 -0500

    added some testing just for starting

--- app/src/main/java/com/example/vitaly/razeroutes/Buse.java
+++ app/src/main/java/com/example/vitaly/razeroutes/Buse.java
@@ -5,4 +5,60 @@ package com.example.vitaly.razeroutes;
  */
 public class Buse
 {
+    private int ID;
+    private int Fleer;
+    private String Name;
+    private String Description;
+    private int ZonarId;
+    private String GpsId;
+    private double Latitude;
+    private double Longitude;
+    private double Speed;
+    private String Heading;
+    private boolean Power;
+    private String Date;
+    private String Color;
+    private String RouteName;
+    private int RouteId;
+    private double Distance;
+    private String NextStop;
+    private String NextArrival;
+
+    public int getId()              { return ID;}
+    public int getFleer()           { return Fleer;}
+    public String getName()         { return Name;}
+    public String getDescription()  { return Description;}
+    public int getZonarId()         { return ZonarId;}
+    public String getGpsId()        { return GpsId;}
+    public double getLatitude()     { return Latitude;}
+    public double getLongitude()    { return Longitude;}
+    public double getSpeed()        { return Speed;}
+    public String getHeading()      { return Heading;}
+    public boolean getPower()       { return Power;}
+    public String getDate()         { return Date;}
+    public String getColor()        { return Color;}
+    public String getRouteName()    { return RouteName;}
+    public int getRouteId()         { return RouteId;}
+    public double getDistance()     { return Distance;}
+    public String getNextStop()     { return NextStop;}
+    public String getNextArrival()  { return NextArrival;}
+
+    public void setId(int id)                       { ID = id;}
+    public void setFleer(int fleer)                 { Fleer = fleer;}
+    public void setName(String name)                { Name = name;}
+    public void setDescription(String description)  { Description = description;}
+    public void setZonarId(int zonarId)             { ZonarId = zonarId;}
+    public void setGpsId(String gpsId)              { GpsId = gpsId;}
+    public void setLatitude(double latitude)        { Latitude = latitude;}
+    public void setLongitude(double longitude)      { Longitude = longitude;}
+    public void setSpeed(double speed)              { Speed = speed;}
+    public void setHeading(String heading)          { Heading = heading;}
+    public void setPower(boolean power)             { Power = power;}
+    public void setDate(String date)                { Date = date;}
+    public void setColor(String color)              { Color = color;}
+    public void setRouteName(String routeName)      { RouteName = routeName;}
+    public void setRouteId(int routeId)             { RouteId = routeId;}
+    public void setDistance(double distance)        { Distance = distance;}
+    public void setNextStop(String nextStop)        { NextStop = nextStop;}
+    public void setNextArrival(String nextArrival)  { NextArrival = nextArrival;}
 }
---
+++ app/src/main/java/com/example/vitaly/razeroutes/JSONTask.java
@@ -0,0 +1,134 @@
+package com.example.vitaly.razeroutes;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by borodin on 4/15/2016.
+ */
+class JSONTask extends AsyncTask<String , String, String>
+{
+    TextView output = null;
+
+    JSONTask (TextView tv)
+    {
+        output = tv;
+    }
+    private final static String TAG = "TESTING";
+    @Override
+    protected String doInBackground(String ... params)
+    {
+        Buse buse = new Buse();
+        StringBuffer retValBuffer = new StringBuffer();
+        String finalJason = getdata(params[0]);
+        try
+        {
+                /*
+                This is cove staff for the array but so far I use only singel objekt.
+                 */
+            // JSONObject parentObject = new JSONObject(finalJason);
+            // JSONArray parentArray = parentObject.getJSONArray("");
+
+            //for (int i=0; i<parentArray.length(); i++)
+            //{
+            //JSONObject finalObject = parentArray.getJSONObject(i);
+            JSONObject finalObject = new JSONObject(finalJason);
+            buse.setId(finalObject.getInt("id"));
+            buse.setFleer(finalObject.getInt("fleet"));
+            buse.setName(finalObject.getString("name"));
+            buse.setDescription(finalObject.getString("description"));
+            buse.setZonarId(finalObject.getInt("zonarId"));
+            buse.setGpsId(finalObject.getString("gpsId"));
+            buse.setLatitude(finalObject.getDouble("latitude"));
+            buse.setLongitude(finalObject.getDouble("longitude"));
+            buse.setSpeed(finalObject.getDouble("speed"));
+            buse.setColor(finalObject.getString("color"));
+
+            String retval =  "ID = " + buse.getId() + "\n" +
+                    "Fleet = " + buse.getFleer() + "\n" +
+                    "Name = " + buse.getName() + "\n" +
+                    "Description = " + buse.getDescription() + "\n" +
+                    "ZonarID = " + buse.getZonarId() + "\n" +
+                    "GPS ID = " + buse.getGpsId() + "\n" +
+                    "Latitude = " + buse.getLatitude() + "\n" +
+                    "Longitude = " + buse.getLongitude() + "\n" +
+                    "Speed = " + buse.getSpeed() + "\n" +
+                    "Color = " + buse.getColor();
+
+            retValBuffer.append(retval);
+            //}
+
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+        // return finalJason;
+        return retValBuffer.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String s)
+    {
+        output.setText(s);
+    }
+
+    private String getdata(String urlstr)
+    {
+
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        StringBuffer buffer = new StringBuffer();
+        try
+        {
+            URL url = new URL(urlstr);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            String line = "";
+            while ((line = reader.readLine()) != null)
+            {
+                line = line.replace("?([{", "{");
+                line = line.replace("}]);", "}");
+                buffer.append(line);
+            }
+            Log.d(TAG, buffer.toString());
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (connection != null) connection.disconnect();
+            try
+            {
+                if (reader != null) reader.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return buffer.toString();
+    }
+}
