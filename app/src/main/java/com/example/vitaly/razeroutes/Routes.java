ce0ec022a8c8cfd1c7f03140a13e559d20b0cb1b
026rus <026rus@gmail.com>
2016-04-24 12:30:09 -0500

    testr rout

---
+++ app/src/main/java/com/example/vitaly/razeroutes/Routes.java
@@ -0,0 +1,16 @@
+package com.example.vitaly.razeroutes;
+
+/**
+ * Created by vitaly on 4/24/16.
+ */
+
+
+public class Routes
+{
+
+    int ID;
+    String name;
+    String Description;
+    String Color;
+
+}
